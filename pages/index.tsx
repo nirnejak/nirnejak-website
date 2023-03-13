0fb6885cacbf57098d89d040e992616916e446bd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-14 00:26:50 +0530

    Custom Font Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,18 @@ import Head from "next/head"
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-zinc-900">
       <Head>
-        <title>Create Next App</title>
+        <title>Jitendra Nirnejak</title>
         <meta name="description" content="Next.js Typescript Starter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="grid h-screen place-content-center">
-        <h1 className="text-center text-8xl font-bold text-slate-800">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="flex gap-1.5 text-center text-5xl font-bold text-zinc-800">
+          <span className="text-gradient-1">Design,</span>
+          <span className="text-gradient-2">Code & </span>
+          <span className="text-gradient-3">Write.</span>
         </h1>
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      sans: ["Inter"],
+      sans: ["Space Grotesk"],
     },
   },
   variants: {
