c800021168400343deed4283e849bf732abf4462
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-14 15:40:15 +0530

    _document.tsx added with metadata

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+const MyDocument = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        {/* Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+        <meta name="theme-color" content="#00d09f" />
+        {/* Basic Meta */}
+        <meta
+          name="keywords"
+          content="Designer, Developer, Content, Blogging, React, TypeScript, Node, JavaScript, Postgres"
+        />
+        <meta name="author" content="Jitendra Nirnejak" />
+        <meta name="application-name" content="Jitendra Nirnejak" />
+        {/* Schema.org for Google */}
+        <meta itemProp="name" content="Jitendra Nirnejak" />
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={"Jitendra Nirnejak"} />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@jeetnirnejak" />
+        <meta name="twitter:site" content="@jeetnirnejak" />
+      </Head>
+      <body className="overflow-x-hidden bg-neutral-50">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default MyDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import Head from "next/head"
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div className="bg-zinc-900">
       <Head>
@@ -13,13 +13,13 @@ const Home = () => {
 
       <main className="grid h-screen place-content-center">
         <h1 className="flex gap-1.5 text-center text-5xl font-bold text-zinc-800">
-          <span className="text-gradient-1">Design,</span>
-          <span className="text-gradient-2">Code & </span>
-          <span className="text-gradient-3">Write.</span>
+          <span className="text-gradient-blue">Design,</span>
+          <span className="text-gradient-red">Code & </span>
+          <span className="text-gradient-yellow">Write.</span>
         </h1>
       </main>
     </div>
   )
 }
 
-export default Home
+export default HomePage
