3974e64158134c7c27a0ec64bbf26564c93f1026
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-16 19:08:36 +0530

    Background Grain Effect Added

---
+++ components/atoms/Background.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+
+const Background: React.FC = () => {
+  return (
+    <svg
+      className="pointer-events-none fixed top-0 left-0 z-[999] inline-block w-full align-middle opacity-25"
+      style={{
+        height: "calc(100vh + 200px)",
+        transform: "translateY(0px)",
+        filter: "contrast(30%) brightness(70%)",
+      }}
+    >
+      <filter id="noise">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency=".8"
+          numOctaves="4"
+          stitchTiles="stitch"
+        />
+        <feColorMatrix type="saturate" values="0" />
+      </filter>
+      <rect width="100%" height="100%" filter="url(#noise)" />
+    </svg>
+  )
+}
+
+export default Background
