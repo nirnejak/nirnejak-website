c9eaa16a04d2a6a86f98f21dd135005c87b88178
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-16 20:02:24 +0530

    Home: content updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
 import * as React from "react"
 
+import Container from "components/atoms/Container"
 import Layout from "components/atoms/Layout"
 
 const HomePage = () => {
   return (
     <Layout>
-      <main className="grid h-screen place-content-center">
-        <h1 className="flex gap-1.5 text-center text-5xl font-bold text-zinc-800">
-          <span className="text-gradient-blue">Design,</span>
-          <span className="text-gradient-red">Code & </span>
-          <span className="text-gradient-yellow">Write.</span>
-        </h1>
-      </main>
+      <Container>
+        <section className="flex h-screen items-center">
+          <div>
+            <h1 className="text-5xl font-bold text-zinc-800">
+              <span className="text-gradient-blue">Design, </span>
+              <span className="text-gradient-red">Code & </span>
+              <span className="text-gradient-yellow">Write.</span>
+            </h1>
+            <p className="mt-5 text-gray-500">
+              I am passionate about elegant user interfaces, web animations, and
+              data visualization. #figma #react #typescript. Currently{" "}
+              <span className="font-semibold text-slate-400">
+                Inkoop(Draxlr)
+              </span>
+            </p>
+          </div>
+        </section>
+      </Container>
     </Layout>
   )
 }
