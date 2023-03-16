87f5bce685e273bd7c6096a20dee08087fc94e62
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-16 20:30:40 +0530

    Home: work link added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import Container from "components/atoms/Container"
 import Layout from "components/atoms/Layout"
+import AppLink from "components/atoms/Link"
 
 const HomePage = () => {
   return (
@@ -14,12 +15,16 @@ const HomePage = () => {
               <span className="text-gradient-red">Code & </span>
               <span className="text-gradient-yellow">Write.</span>
             </h1>
-            <p className="mt-5 text-gray-500">
+            <p className="mt-5 font-light text-gray-500">
               I am passionate about elegant user interfaces, web animations, and
               data visualization. #figma #react #typescript. Currently{" "}
-              <span className="font-semibold text-slate-400">
-                Inkoop(Draxlr)
-              </span>
+              <AppLink
+                href="https://www.draxlr.com/"
+                target="_blank"
+                className="font-normal text-slate-400"
+              >
+                @Inkoop(Draxlr)
+              </AppLink>
             </p>
           </div>
         </section>
