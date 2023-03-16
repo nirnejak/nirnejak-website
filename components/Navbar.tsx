6035d12faf7e3456d771172b2484c8f317baea66
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-16 19:13:40 +0530

    Navbar Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 
+import Container from "./atoms/Container"
 import AppLink from "./atoms/Link"
 
 type Props = {
@@ -8,17 +9,19 @@ type Props = {
 
 const Navbar: React.FC<Props> = () => {
   return (
-    <nav className="fixed top-0 flex w-full px-10 py-6">
-      <div className="flex gap-5">
-        <AppLink href="/">Home</AppLink>
-        <AppLink href="/">Blogs</AppLink>
-        <AppLink href="/">Work</AppLink>
-        <AppLink href="/">Uses</AppLink>
-      </div>
-      <div className="ml-auto flex gap-5">
-        <AppLink href="/">Resumé</AppLink>
-        <AppLink href="/">Contact</AppLink>
-      </div>
+    <nav className="fixed top-0 w-full px-10">
+      <Container className="flex py-6">
+        <div className="flex gap-5">
+          <AppLink href="/">Home</AppLink>
+          <AppLink href="/">Blogs</AppLink>
+          <AppLink href="/">Work</AppLink>
+          <AppLink href="/">Uses</AppLink>
+        </div>
+        <div className="ml-auto flex gap-5">
+          <AppLink href="/">Resumé</AppLink>
+          <AppLink href="/">Contact</AppLink>
+        </div>
+      </Container>
     </nav>
   )
 }
