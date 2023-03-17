187cc2bbc16f0aeabb32331fefa33cffd4957ea4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-17 18:41:48 +0530

    Pages Added

---
+++ pages/uses.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+import Container from "components/atoms/Container"
+import Layout from "components/atoms/Layout"
+
+const UsesPage = () => {
+  return (
+    <Layout>
+      <Container>
+        <section className="flex h-screen items-start pt-56">
+          <div>
+            <h1 className="text-5xl font-bold text-zinc-300">Uses</h1>
+          </div>
+        </section>
+      </Container>
+    </Layout>
+  )
+}
+
+export default UsesPage
