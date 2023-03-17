07014eba95bf0920556d7fa731c8848a05fbfdda
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-17 23:48:42 +0530

    Blogs content added

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -2,14 +2,32 @@ import * as React from "react"
 
 import Container from "components/atoms/Container"
 import Layout from "components/atoms/Layout"
+import AppLink from "components/atoms/Link"
+import { allBlogs } from "utils/data"
 
 const BlogsPage = () => {
   return (
     <Layout>
       <Container>
-        <section className="flex h-screen items-start pt-56">
+        <section className="flex min-h-screen items-start pt-56">
           <div>
             <h1 className="text-5xl font-bold text-zinc-300">Blogs</h1>
+
+            <div className="-mx-3 mt-10 flex flex-col gap-1 pb-16">
+              {allBlogs.map((blog, index) => (
+                <AppLink
+                  key={index}
+                  href={blog.url}
+                  target="_blank"
+                  className="flex flex-col rounded-lg p-3 hover:bg-zinc-800"
+                >
+                  <h3 className=" font-bold">{blog.title}</h3>
+                  <p className="mt-3 text-sm font-light text-zinc-500">
+                    {blog.description}
+                  </p>
+                </AppLink>
+              ))}
+            </div>
           </div>
         </section>
       </Container>
