d8a7d81a2c2a70afc5428b1434d784c2f8906ec9
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-19 00:14:49 +0530

    Contact Form Added

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,9 +7,30 @@ const ContactPage: React.FC = () => {
   return (
     <Layout>
       <Container>
-        <section className="flex h-screen items-start pt-56">
-          <div>
+        <section className="flex min-h-screen items-start pt-56">
+          <div className="w-full">
             <h1 className="text-5xl font-bold text-zinc-300">Contact</h1>
+
+            <form className="mt-16 flex w-full flex-col gap-3 pb-16">
+              <input
+                type="text"
+                className="w-full rounded-md bg-zinc-800 py-3 px-4 text-xs text-white"
+                placeholder="Name"
+              />
+              <input
+                type="email"
+                className="w-full rounded-md bg-zinc-800 py-3 px-4 text-xs text-white"
+                placeholder="Email"
+              />
+              <textarea
+                className="w-full rounded-md bg-zinc-800 py-3 px-4 text-xs text-white"
+                placeholder="Message"
+                rows={3}
+              />
+              <button className="rounded-md bg-white py-3 px-4 text-center text-sm font-bold uppercase tracking-wide">
+                Submit
+              </button>
+            </form>
           </div>
         </section>
       </Container>
