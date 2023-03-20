1a3ee89dca67a06ff429ae8964459224aeac9e0e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-21 02:02:39 +0530

    Uses: setup picture added

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -1,5 +1,8 @@
 import * as React from "react"
 
+import Image from "next/image"
+
+import SetupImage from "../assets/images/setup.png"
 import Container from "components/atoms/Container"
 import Layout from "components/atoms/Layout"
 
@@ -7,64 +10,59 @@ const UsesPage: React.FC = () => {
   return (
     <Layout>
       <Container>
-        <section className="flex min-h-screen items-start pt-32 md:pt-56">
+        <section className="flex min-h-screen items-start pt-32 md:pt-40">
           <div className="w-full">
             <h1 className="text-5xl font-bold text-zinc-300">Uses</h1>
 
-            <div className="mt-10 flex flex-col gap-8 pb-16 md:mt-16">
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  Design Tools
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  Figma and Procreate
-                </p>
+            <div className="mt-10 flex gap-12 pb-16 md:mt-16">
+              <div className="flex-1">
+                <Image
+                  src={SetupImage}
+                  alt="Desk Setup"
+                  className="rounded-lg"
+                />
               </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  Terminal and Editor
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  ZSH + Oh-my-zsh, iTerm, VS Code
-                </p>
-              </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  Languages and Frameworks
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  TypeScript, React and Node.js
-                </p>
-              </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">Gear</h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  iPhone 14 Pro, iPad Pro 11, MacBook Air(M2)
-                </p>
-              </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  Podcasts, I listen to
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  Typescript and Testing
-                </p>
-              </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  What was 2022 about
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  Typescript and Testing
-                </p>
-              </div>
-              <div>
-                <h4 className="text-lg font-bold text-zinc-300">
-                  What&apos;s for 2023
-                </h4>
-                <p className="mt-1.5 text-sm text-zinc-500">
-                  Fastify and Prisma
-                </p>
+              <div className="flex flex-1 flex-col gap-8">
+                <div>
+                  <h4 className="text-lg font-bold text-zinc-300">
+                    Design Tools
+                  </h4>
+                  <p className="mt-1.5 text-sm text-zinc-500">
+                    Figma and Procreate
+                  </p>
+                </div>
+                <div>
+                  <h4 className="text-lg font-bold text-zinc-300">
+                    Terminal and Editor
+                  </h4>
+                  <p className="mt-1.5 text-sm text-zinc-500">
+                    ZSH + Oh-my-zsh, iTerm, VS Code
+                  </p>
+                </div>
+                <div>
+                  <h4 className="text-lg font-bold text-zinc-300">
+                    Languages and Frameworks
+                  </h4>
+                  <p className="mt-1.5 text-sm text-zinc-500">
+                    TypeScript, React and Node.js
+                  </p>
+                </div>
+                <div>
+                  <h4 className="text-lg font-bold text-zinc-300">
+                    Gear & Gadgets
+                  </h4>
+                  <p className="mt-1.5 text-sm text-zinc-500">
+                    iPhone 14 Pro, iPad Pro 11, MacBook Air(M2)
+                  </p>
+                </div>
+                <div>
+                  <h4 className="text-lg font-bold text-zinc-300">
+                    What&apos;s for 2023
+                  </h4>
+                  <p className="mt-1.5 text-sm text-zinc-500">
+                    TypeScript & Testing
+                  </p>
+                </div>
               </div>
             </div>
           </div>
