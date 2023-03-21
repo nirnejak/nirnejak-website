8738b53eb75462ba3ee6f618273f88a91dcab685
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-21 21:19:47 +0530

    next-pwa integrated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})
+
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   poweredByHeader: false,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
