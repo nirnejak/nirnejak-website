bed86dcafc5c1a83d93f6ce244dbfc6c2de10c1b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-24 23:24:42 +0530

    navLinks moved to navigation util

--- components/atoms/Tabs.tsx
+++ components/atoms/Tabs.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import Link from "next/link"
 
-import { navLinks } from "components/Navbar"
+import { navLinks } from "utils/navigation"
 
 const tabsData = navLinks
 
---
+++ utils/navigation.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    content: "Home",
+    value: "home",
+    link: "/",
+  },
+  {
+    content: "Work",
+    value: "work",
+    link: "/work/",
+  },
+  {
+    content: "Blogs",
+    value: "blogs",
+    link: "/blogs/",
+  },
+  {
+    content: "Photos",
+    value: "photos",
+    link: "/photos/",
+  },
+  {
+    content: "Uses",
+    value: "uses",
+    link: "/uses/",
+  },
+  {
+    content: "Contact",
+    value: "contact",
+    link: "/contact/",
+  },
+]
