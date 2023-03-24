43b486a514affc35026a88107961225b546659b8
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-24 22:54:54 +0530

    SEO Component Added, _document more metadata added

---
+++ components/SEO.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+
+import Head from "next/head"
+
+interface Props {
+  path: string
+  title: string
+  description: string
+  image?: string
+}
+
+const SEO: React.FC<Props> = ({ path, title, description, image }) => {
+  const baseUrl = "https://nirnejak.com"
+  const metaTitle = title
+  const metaDescription = description
+  const metaImage = image ?? "https://nirnejak.com/images/covers/main.png"
+
+  return (
+    <Head>
+      <title>{title}</title>
+      {/* Basic Meta */}
+      <meta name="image" content={metaImage} />
+      <meta name="description" content={description} />
+      <link rel="canonical" href={`${baseUrl}${path}`} />
+      {/* Schema.org for Google */}
+      <meta itemProp="description" content={metaDescription} />
+      {/* Open Graph */}
+      <meta property="og:url" content={`${baseUrl}${path}`} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaImage} />
+      <meta property="og:image:type" content={"image/png"} />
+      {/* Twitter */}
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={`${baseUrl}${metaImage}`} />
+    </Head>
+  )
+}
+
+SEO.defaultProps = {
+  title: "Jitendra Nirnejak - Portfolio Website",
+  description:
+    "Jitendra Nirnejak is a developer and designer based out of Bangalore, India. He has expertise in React, TypeScript, Node.js, UI Design and Prototyping.",
+}
+
+export default SEO
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,24 +9,36 @@ const MyDocument: React.FC = () => {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        {/* Icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
+        <link rel="mask-icon" href="/icons/icon-512x512.png" color="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
         {/* Manifest */}
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-512x512.png"></link>
-        <meta name="theme-color" content="#00d09f" />
         {/* Basic Meta */}
         <meta
           name="keywords"
           content="Designer, Developer, Content, Blogging, React, TypeScript, Node, JavaScript, Postgres"
         />
         <meta name="author" content="Jitendra Nirnejak" />
+        <meta
+          name="robots"
+          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+        />
         <meta name="application-name" content="Jitendra Nirnejak" />
         {/* Schema.org for Google */}
         <meta itemProp="name" content="Jitendra Nirnejak" />
         {/* Open Graph */}
+        <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={"Jitendra Nirnejak"} />
+        {/* Facebook App */}
+        <meta property="fb:admins" content="100003147340678" />
+        <meta property="fb:app_id" content="303711933555596" />
         {/* Twitter */}
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@jeetnirnejak" />
         <meta name="twitter:site" content="@jeetnirnejak" />
       </Head>
