252ba4caaf8dd51f6064c5cef2c0b99d3f36b8aa
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-24 23:18:08 +0530

    Blogs: responsiveness

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -45,15 +45,15 @@ const BlogsPage: React.FC = () => {
           <h1 className="text-5xl font-bold text-zinc-300">Blogs</h1>
 
           <div className="mt-10 flex flex-col gap-10 pb-16 md:mt-16">
-            <div className="flex gap-24">
-              <div className="mt-3 text-sm text-zinc-300">2022</div>
-              <div className="mx-3 flex flex-col gap-1">
+            <div className="flex flex-col gap-12 md:flex-row md:gap-24">
+              <div className="text-sm text-zinc-300 md:mt-3">2022</div>
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allBlogs[2022].map((blog, index) => (
                   <AppLink
                     key={index}
                     href={blog.url}
                     target="_blank"
-                    className="hover-bg p-3"
+                    className="hover-bg -mx-3 p-3 md:mx-0"
                   >
                     <h3 className="text-sm font-bold">{blog.title}</h3>
                     <p className="mt-1.5 text-xs text-zinc-500">
@@ -63,15 +63,15 @@ const BlogsPage: React.FC = () => {
                 ))}
               </div>
             </div>
-            <div className="flex gap-24">
-              <div className="mt-3 text-sm text-zinc-300">2021</div>
-              <div className="mx-3 flex flex-col gap-1">
+            <div className="flex flex-col gap-12 md:flex-row md:gap-24">
+              <div className="text-sm text-zinc-300 md:mt-3">2021</div>
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allBlogs[2021].map((blog, index) => (
                   <AppLink
                     key={index}
                     href={blog.url}
                     target="_blank"
-                    className="hover-bg p-3"
+                    className="hover-bg -mx-3 p-3 md:mx-0"
                   >
                     <h3 className="text-sm font-bold">{blog.title}</h3>
                     <p className="mt-1.5 text-xs text-zinc-500">
@@ -81,15 +81,15 @@ const BlogsPage: React.FC = () => {
                 ))}
               </div>
             </div>
-            <div className="flex gap-24">
-              <div className="mt-3 text-sm text-zinc-300">2020</div>
-              <div className="mx-3 flex flex-col gap-1">
+            <div className="flex flex-col gap-12 md:flex-row md:gap-24">
+              <div className="text-sm text-zinc-300 md:mt-3">2020</div>
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allBlogs[2020].map((blog, index) => (
                   <AppLink
                     key={index}
                     href={blog.url}
                     target="_blank"
-                    className="hover-bg p-3"
+                    className="hover-bg -mx-3 p-3 md:mx-0"
                   >
                     <h3 className="text-sm font-bold">{blog.title}</h3>
                     <p className="mt-1.5 text-xs text-zinc-500">
@@ -99,15 +99,15 @@ const BlogsPage: React.FC = () => {
                 ))}
               </div>
             </div>
-            <div className="flex gap-24">
-              <div className="mt-3 text-sm text-zinc-300">2019</div>
-              <div className="mx-3 flex flex-col gap-1">
+            <div className="flex flex-col gap-12 md:flex-row md:gap-24">
+              <div className="text-sm text-zinc-300 md:mt-3">2019</div>
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allBlogs[2019].map((blog, index) => (
                   <AppLink
                     key={index}
                     href={blog.url}
                     target="_blank"
-                    className="hover-bg p-3"
+                    className="hover-bg -mx-3 p-3 md:mx-0"
                   >
                     <h3 className="text-sm font-bold">{blog.title}</h3>
                     <p className="mt-1.5 text-xs text-zinc-500">
