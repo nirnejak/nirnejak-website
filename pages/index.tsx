98fb6f9714e8e901d8fe76cdc615e4e919049b54
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-25 03:53:07 +0530

    Home: initial load animation removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import * as React from "react"
-import { useInView } from "react-intersection-observer"
-
-import { motion, useAnimation } from "framer-motion"
 
 import Container from "components/atoms/Container"
 import AppLink from "components/atoms/Link"
@@ -13,22 +10,6 @@ import SocialLinks from "components/SocialLinks"
 // TODO: maybe add recently done projects
 
 const HomePage: React.FC = () => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  React.useEffect(() => {
-    if (inView) {
-      controls.start("visible").catch((err) => {
-        console.log(err)
-      })
-    }
-  }, [controls, inView])
-
-  const variants = {
-    visible: { opacity: 1, translateY: 0 },
-    hidden: { opacity: 0, translateY: 10 },
-  }
-
   return (
     <Container>
       <SEO
@@ -37,13 +18,7 @@ const HomePage: React.FC = () => {
         path="/"
       />
       <section className="flex h-screen items-start pt-32 md:pt-56">
-        <motion.div
-          animate={controls}
-          initial="hidden"
-          transition={{ delay: 0, duration: 0.2 }}
-          variants={variants}
-          ref={ref}
-        >
+        <div>
           <h1 className="text-4xl font-bold text-zinc-300 md:text-5xl">
             Jitendra Nirnejak
           </h1>
@@ -64,7 +39,7 @@ const HomePage: React.FC = () => {
           <div className="mt-16 flex">
             <SocialLinks />
           </div>
-        </motion.div>
+        </div>
       </section>
     </Container>
   )
