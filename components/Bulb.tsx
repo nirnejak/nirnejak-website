948d62c4fb537f7a913fde2e75a6f29f3cd6e09c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-26 22:07:54 +0530

    Bulb: css classes to tailwind

--- components/Bulb.tsx
+++ components/Bulb.tsx
@@ -2,8 +2,6 @@ import * as React from "react"
 
 import useSound from "use-sound"
 
-import classNames from "utils/classNames"
-
 const Bulb: React.FC = () => {
   const [isSwitchOn, setIsSwitchOn] = React.useState(false)
 
@@ -42,9 +40,15 @@ const Bulb: React.FC = () => {
     <div className="fixed top-0 right-24 hidden h-[500px] w-52 origin-top animate-swing md:block">
       <div className="relative left-[98px] h-[200px] w-1 bg-black" />
       <div className="relative left-[92px] h-[20px] w-4 bg-zinc-500">
-        <div className="strip" />
-        <div className="strip" />
-        <div className="strip" />
+        <div className="relative top-1 right-[1px] h-[2px] w-[18px] bg-zinc-400" />
+        <div
+          className="relative top-1 right-[1px] h-[2px] w-[18px] bg-zinc-400"
+          style={{ top: 2 }}
+        />
+        <div
+          className="relative top-1 right-[1px] h-[2px] w-[18px] bg-zinc-400"
+          style={{ top: 10 }}
+        />
       </div>
       {isBroken ? (
         ""
@@ -52,7 +56,15 @@ const Bulb: React.FC = () => {
         <div
           tabIndex={0}
           role={"button"}
-          className={classNames("bulb", isSwitchOn ? "on" : "")}
+          className="relative left-[80px] bottom-[2px] z-[-1] h-[40px] w-[40px] cursor-pointer rounded-full bg-zinc-700"
+          style={
+            isSwitchOn
+              ? {
+                  boxShadow: "0px 0px 300px 77px rgba(235, 209, 164, 1)",
+                  backgroundColor: "rgba(226, 211, 161, 0.85)",
+                }
+              : {}
+          }
           onKeyUp={(e) => {
             e.key === "enter" && setIsSwitchOn(!isSwitchOn)
           }}
@@ -60,9 +72,19 @@ const Bulb: React.FC = () => {
             toggleBulb()
           }}
         >
-          <div className="zig" />
-          <div className="zig" />
-          <div className="zig" />
+          {Array(5)
+            .fill(1)
+            .map((item, index) => (
+              <div
+                key={index}
+                style={{
+                  borderRadius: "5px / 2.5px",
+                  border: "black solid 1px",
+                  top: index * -3,
+                }}
+                className="relative left-[14px] h-[5px] w-[10px]"
+              />
+            ))}
         </div>
       )}
     </div>
