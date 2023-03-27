e647e4fb34f999828a2e7cdf7023b1b2313a90a4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-27 19:38:11 +0530

    Home: spring animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const HomePage: React.FC = () => {
         <motion.div
           animate={controls}
           initial="hidden"
-          transition={{ delay: 0, duration: 0.15 }}
+          transition={{ delay: 0, duration: 0.15, type: "spring" }}
           variants={variants}
           ref={ref}
         >
