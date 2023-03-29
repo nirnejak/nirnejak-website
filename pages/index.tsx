cb5172ea16de78230e16f21e7cf8dfd4fb3fe9c4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-03-30 02:20:21 +0530

    Home: animation and intersection observer added back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
+import { useInView } from "react-intersection-observer"
 
-import { motion } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
 
 import Container from "components/atoms/Container"
 import AppLink from "components/atoms/Link"
@@ -12,6 +13,22 @@ import SocialLinks from "components/SocialLinks"
 // TODO: maybe add recently done projects
 
 const HomePage: React.FC = () => {
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start("visible").catch((err) => {
+        console.log(err)
+      })
+    }
+  }, [controls, inView])
+
+  const variants = {
+    visible: { opacity: 1, translateY: 0 },
+    hidden: { opacity: 0, translateY: 10 },
+  }
+
   return (
     <Container>
       <SEO
@@ -21,9 +38,11 @@ const HomePage: React.FC = () => {
       />
       <section className="flex h-screen items-start pt-32 md:pt-56">
         <motion.div
-        // animate={{ opacity: 1, translateY: 0 }}
-        // initial={{ opacity: 0, translateY: 10 }}
-        // transition={{ delay: 0, duration: 0.15, type: "spring" }}
+          animate={controls}
+          initial="hidden"
+          transition={{ delay: 0, duration: 0.15, type: "spring" }}
+          variants={variants}
+          ref={ref}
         >
           <h1 className="text-4xl font-bold text-zinc-300 md:text-5xl">
             Jitendra Nirnejak
