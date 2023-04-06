9934024a0d8c334c6f0e4fde5f79f2856b846323
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-06 23:23:25 +0530

    Photos: reveal animation removed

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,7 +1,4 @@
 import * as React from "react"
-import { useInView } from "react-intersection-observer"
-
-import { motion, useAnimation } from "framer-motion"
 
 import Container from "components/atoms/Container"
 import SEO from "components/SEO"
@@ -9,22 +6,6 @@ import SEO from "components/SEO"
 // TODO: Photo gallery and preview
 
 const PhotosPage: React.FC = () => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  React.useEffect(() => {
-    if (inView) {
-      controls.start("visible").catch((err) => {
-        console.log(err)
-      })
-    }
-  }, [controls, inView])
-
-  const variants = {
-    visible: { opacity: 1, translateY: 0 },
-    hidden: { opacity: 0, translateY: 10 },
-  }
-
   return (
     <Container>
       <SEO
@@ -33,20 +14,13 @@ const PhotosPage: React.FC = () => {
         path="/photos/"
       />
       <section className="flex min-h-screen items-start pt-32 md:pt-40">
-        <motion.div
-          animate={controls}
-          initial="hidden"
-          transition={{ delay: 0, duration: 0.15, type: "spring" }}
-          variants={variants}
-          ref={ref}
-          className="w-full"
-        >
+        <div className="w-full">
           <h1 className="text-5xl font-bold text-zinc-300">Photos</h1>
 
           <div className="mt-10 flex flex-col gap-1 pb-16 md:mt-16">
             <p className="my-16 text-center text-zinc-500">Soon ✨</p>
           </div>
-        </motion.div>
+        </div>
       </section>
     </Container>
   )
