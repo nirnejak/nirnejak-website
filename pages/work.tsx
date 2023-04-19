5113206237c4fdb620253446b458842f8c7de7b5
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-19 23:40:35 +0530

    Work: layout adjustments

--- pages/work.tsx
+++ pages/work.tsx
@@ -20,15 +20,15 @@ const WorkPage: React.FC = () => {
           <h1 className="text-5xl font-bold text-zinc-300">Work</h1>
 
           <div className="mt-10 text-sm md:mt-16">
-            <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="mb-16 grid grid-cols-1 md:grid-cols-2">
               <p className="mb-3 text-zinc-300 md:mt-2">Side Projects</p>
-              <div className="-mx-3 flex flex-col gap-1 pb-16">
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allProjects.work.map((project, index) => (
                   <AppLink
                     key={index}
                     href={project.link}
                     target="_blank"
-                    className="hover-bg rounded-md px-3 py-2 outline-none md:text-base "
+                    className="hover-bg rounded-md px-3 py-2 outline-none"
                   >
                     <p className="mb-1.5 font-bold">{project.title}</p>
                     <p className="text-xs text-zinc-500">
@@ -38,15 +38,15 @@ const WorkPage: React.FC = () => {
                 ))}
               </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="mb-16 grid grid-cols-1 md:grid-cols-2">
               <p className="mb-3 text-zinc-300 md:mt-2">Work Projects</p>
-              <div className="-mx-3 flex flex-col gap-1 pb-16">
+              <div className="flex flex-col gap-1 md:mx-3">
                 {allProjects.side.map((project, index) => (
                   <AppLink
                     key={index}
                     href={project.link}
                     target="_blank"
-                    className="hover-bg rounded-md px-3 py-2 outline-none md:text-base "
+                    className="hover-bg rounded-md px-3 py-2 outline-none"
                   >
                     <p className="mb-1.5 font-bold">{project.title}</p>
                     <p className="text-xs text-zinc-500">
