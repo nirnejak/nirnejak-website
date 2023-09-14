66d942f5271aecc6179e19e5d3cc8991a9bb3ac3
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-09-14 22:12:06 +0530

    Photos added

--- utils/photos.ts
+++ utils/photos.ts
@@ -14,5 +14,14 @@ export const getAllImages = (): string[] => {
     .filter((slug: string) => !slug.includes(".DS_Store"))
     .filter((slug: string) => !slug.includes(".HEIC"))
     .filter((slug: string) => !slug.includes(".heic"))
+    .filter((slug: string) => !slug.includes("IMG_0135"))
+    .filter((slug: string) => !slug.includes("IMG_7217"))
+    .filter((slug: string) => !slug.includes("IMG_7214"))
+    .filter((slug: string) => !slug.includes("IMG_0807"))
+    .filter((slug: string) => !slug.includes("IMG_4671"))
+    .filter((slug: string) => !slug.includes("IMG_6442"))
+    .filter(
+      (slug: string) => !slug.includes("EFF085DC-8EF6-4AC4-8E8B-714C1B20F6F5")
+    )
   return images
 }
