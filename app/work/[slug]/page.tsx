241ae0368b74fa33e4d9da7cf0ba6e0568a3f486
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-04 23:14:55 +0530

    Work details page fixes

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -1,14 +1,15 @@
 import * as React from "react"
+
 import { type Metadata } from "next"
-import { redirect } from "next/navigation"
 
 import { LinkOut } from "akar-icons"
+import { redirect } from "next/navigation"
 
-import generateMetadata from "utils/seo"
-import { allProjects } from "utils/data"
 import Container from "components/atoms/Container"
 import AppLink from "components/atoms/Link"
 import ProjectImagesGallery from "components/ProjectImagesGallery"
+import { allProjects } from "utils/data"
+import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
   path: "/work/",
@@ -16,7 +17,7 @@ export const metadata: Metadata = generateMetadata({
   description: "Projects built by Jitendra Nirnejak",
 })
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<Array<{ slug: string }>> {
   return allProjects.work.map((post) => ({
     slug: post.slug,
   }))
@@ -33,21 +34,21 @@ const WorkDetailsPage: React.FC<Props> = ({ params }) => {
     (project) => project.slug === params.slug
   )
 
-  if (!project) {
+  if (project === undefined) {
     redirect("/404")
   } else {
     return (
       <>
         <Container>
-          <section className="pt-32 md:pt-40 w-full flex">
-            <div className="flex-1 flex flex-col">
-              <h1 className="text-3xl font-bold text-zinc-300 mb-3">
+          <section className="flex w-full pt-32 md:pt-40">
+            <div className="flex flex-1 flex-col">
+              <h1 className="mb-3 text-3xl font-bold text-zinc-300">
                 {project.title}
               </h1>
-              <p className="text-sm text-zinc-500">{project.type}</p>
-              <div className="flex -ml-4 -mb-2 mt-24">
+              <p className="text-sm text-zinc-400">{project.type}</p>
+              <div className="-mb-2 -ml-4 mt-24 flex">
                 <AppLink
-                  className="font-medium text-xs rounded-md px-4 py-2 hover-bg outline-none group flex items-center gap-1.5"
+                  className="hover-bg group flex items-center gap-1.5 rounded-md px-4 py-2 text-xs font-medium outline-none"
                   href={project.link}
                   target="_blank"
                 >
@@ -59,21 +60,21 @@ const WorkDetailsPage: React.FC<Props> = ({ params }) => {
                 </AppLink>
               </div>
             </div>
-            <div className="flex-1 text-sm text-zinc-500">
+            <div className="flex-1 text-sm text-zinc-400">
               <p className="mb-6">{project.description}</p>
-              <p className="font-semibold mb-2 text-zinc-300">Contributions</p>
-              <ul className="list-disc gap-2.5 flex flex-col ml-4">
-                {project.contributions?.map((contribution) => (
-                  <li>{contribution}</li>
+              <p className="mb-2 font-semibold text-zinc-300">Contributions</p>
+              <ul className="ml-4 flex list-disc flex-col gap-2.5">
+                {project.contributions?.map((contribution, index) => (
+                  <li key={index}>{contribution}</li>
                 ))}
               </ul>
             </div>
           </section>
         </Container>
         <ProjectImagesGallery images={project.images} />
-        <div className="flex justify-center mb-28">
+        <div className="mb-28 flex justify-center">
           <AppLink
-            className="font-medium text-xs rounded-md px-4 py-2 hover-bg outline-none group flex items-center gap-1.5"
+            className="hover-bg group flex items-center gap-1.5 rounded-md px-4 py-2 text-xs font-medium outline-none"
             href={project.link}
             target="_blank"
           >
