47d7c2a5becda39122957dd549137889f7bbc80a
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-16 17:12:47 +0530

    Photo Gallery: reveal animation updated

--- components/PhotoGallery.tsx
+++ components/PhotoGallery.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 
 import { XSmall } from "akar-icons"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 import classNames from "utils/classNames"
@@ -56,33 +56,31 @@ const PhotoGallery: React.FC<Props> = ({ images }) => {
 
   return (
     <>
-      <AnimatePresence>
-        {isModalVisible && (
-          <div className="fixed left-0 top-0 z-20 grid h-screen w-full place-items-center bg-zinc-900/30 backdrop-blur-lg">
-            <button
-              className="fixed right-5 top-5 rounded-full bg-zinc-700 p-1.5 text-zinc-300 hover:bg-zinc-500"
-              onClick={() => {
-                closeModal()
-              }}
-            >
-              <XSmall />
-            </button>
-            <motion.div
-              initial={{ opacity: 0, translateY: 15 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              exit={{ opacity: 0 }}
-              className="flex p-5"
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={currentImage}
-                alt={currentImage}
-                className="h-[calc(100vh-50px)] rounded-2xl"
-              />
-            </motion.div>
-          </div>
-        )}
-      </AnimatePresence>
+      {isModalVisible && (
+        <div className="fixed left-0 top-0 z-20 grid h-screen w-full place-items-center bg-zinc-900/30 backdrop-blur-lg">
+          <button
+            className="fixed right-5 top-5 rounded-full bg-zinc-700 p-1.5 text-zinc-300 hover:bg-zinc-500"
+            onClick={() => {
+              closeModal()
+            }}
+          >
+            <XSmall />
+          </button>
+          <motion.div
+            initial={{ opacity: 0, translateY: 15 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0 }}
+            className="flex p-5"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={currentImage}
+              alt={currentImage}
+              className="h-[calc(100vh-50px)] rounded-2xl"
+            />
+          </motion.div>
+        </div>
+      )}
       <div className="-mx-5 mb-5 hidden flex-wrap gap-1">
         {allTags.map((tag, index) => (
           <button
@@ -100,37 +98,36 @@ const PhotoGallery: React.FC<Props> = ({ images }) => {
         ))}
       </div>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
-        <AnimatePresence>
-          {images.map((imageUrl, index) => (
-            <motion.div
-              key={index}
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                openModal(imageUrl)
-              }}
-              onKeyUp={(e) => {
-                e.key === "Enter" && openModal(imageUrl)
-              }}
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.9 }}
-              transition={{
-                type: "spring",
-                stiffness: 350,
-                damping: 25,
-              }}
-            >
-              <Image
-                src={imageUrl}
-                alt={imageUrl}
-                className="rounded-2xl"
-                width="360"
-                height="640"
-              />
-            </motion.div>
-          ))}
-        </AnimatePresence>
+        {images.map((imageUrl, index) => (
+          <motion.div
+            key={index}
+            role="button"
+            tabIndex={0}
+            onClick={() => {
+              openModal(imageUrl)
+            }}
+            onKeyUp={(e) => {
+              e.key === "Enter" && openModal(imageUrl)
+            }}
+            initial={{ scale: 0.9, rotate: "5deg", opacity: 0 }}
+            animate={{ scale: 1, rotate: "0deg", opacity: 1 }}
+            transition={{
+              delay: 0.1 * (index + 1),
+              type: "spring",
+              stiffness: 350,
+              damping: 25,
+              duration: 0.15,
+            }}
+          >
+            <Image
+              src={imageUrl}
+              alt={imageUrl}
+              className="rounded-2xl"
+              width="360"
+              height="640"
+            />
+          </motion.div>
+        ))}
       </div>
     </>
   )
