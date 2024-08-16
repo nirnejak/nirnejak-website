f622ef6035c37d4f96d7e903baa508a3e0cdb918
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-16 16:52:03 +0530

    Tabs: atomic component added

--- components/atoms/Tabs.tsx
+++ components/atoms/Tabs.tsx
@@ -1,100 +1,43 @@
+"use client"
 import * as React from "react"
 
-import Link from "next/link"
-import useSound from "use-sound"
+import useLinkHighlight from "hooks/useLinkHighlight"
+import classNames from "utils/classNames"
 
-import { navLinks } from "utils/navigation"
-
-const tabsData = navLinks
-
-interface TAB_BOUNDING_BOX_TYPE {
-  left: number
-  width: number
+interface Props {
+  tabsOptions: string[]
+  activeTab: number
+  setActiveTab: (value: number) => void
+  className?: string
 }
 
-interface TAB_TYPE {
-  content: string
-  value: string
-  link: string
-}
-
-const Tabs: React.FC = () => {
-  const [play] = useSound("../sounds/whoop.wav")
-
-  const [tabBoundingBox, setTabBoundingBox] =
-    React.useState<TAB_BOUNDING_BOX_TYPE | null>(null)
-  const [wrapperBoundingBox, setWrapperBoundingBox] =
-    React.useState<TAB_BOUNDING_BOX_TYPE | null>(null)
-  const [highlightedTab, setHighlightedTab] = React.useState<TAB_TYPE | null>(
-    null
-  )
-  const [isHoveredFromNull, setIsHoveredFromNull] = React.useState(true)
-
-  const highlightRef = React.useRef(null)
-  const wrapperRef = React.useRef<HTMLDivElement>(null)
-
-  const repositionHighlight = (
-    e:
-      | React.MouseEvent<HTMLAnchorElement, MouseEvent>
-      | React.FocusEvent<HTMLAnchorElement>,
-    tab: TAB_TYPE
-  ): void => {
-    setTabBoundingBox((e.target as HTMLAnchorElement).getBoundingClientRect())
-    if (wrapperRef?.current != null)
-      setWrapperBoundingBox(wrapperRef.current.getBoundingClientRect())
-    setIsHoveredFromNull(highlightedTab == null)
-    setHighlightedTab(tab)
-  }
-
-  const resetHighlight = (): void => {
-    setHighlightedTab(null)
-  }
-
-  const highlightStyles: React.CSSProperties = {}
-
-  if (tabBoundingBox != null && wrapperBoundingBox != null) {
-    highlightStyles.transitionDuration = isHoveredFromNull ? "0ms" : "150ms"
-    highlightStyles.opacity = `${highlightedTab != null ? 1 : 0}`
-    highlightStyles.width = `${tabBoundingBox.width}px`
-    highlightStyles.transform = `translate(${
-      tabBoundingBox.left - wrapperBoundingBox.left
-    }px)`
-  }
+const Tabs: React.FC<Props> = ({
+  tabsOptions,
+  activeTab,
+  setActiveTab,
+  className,
+}) => {
+  const { wrapperRef, highlightStyles } = useLinkHighlight(activeTab)
 
   return (
     <nav
-      className="relative flex gap-1"
+      className={classNames("relative flex gap-1", className)}
       ref={wrapperRef}
-      onMouseLeave={resetHighlight}
     >
       <div
-        className="absolute left-0 h-full rounded-md bg-zinc-800"
-        ref={highlightRef}
-        style={{
-          transition: "0.15s ease",
-          transitionProperty: "width, transform, opacity",
-          ...highlightStyles,
-        }}
+        style={highlightStyles}
+        className="absolute left-0 h-full rounded-lg bg-zinc-800 transition-all"
       />
-      {tabsData.map((tab) => (
-        <Link
-          href={tab.link}
-          key={tab.value}
-          className="relative inline-block px-4 py-2 text-xs font-medium text-white outline-none active:scale-95"
-          onMouseOver={(
-            ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-          ) => {
-            repositionHighlight(ev, tab)
-          }}
-          onFocus={(ev: React.FocusEvent<HTMLAnchorElement>) => {
-            repositionHighlight(ev, tab)
-          }}
+      {tabsOptions.map((tab, index) => (
+        <button
+          key={index}
+          className="relative inline-block rounded-lg px-3.5 py-1.5 outline-none"
           onClick={() => {
-            play()
+            setActiveTab(index)
           }}
         >
-          {tab.content}
-        </Link>
+          {tab}
+        </button>
       ))}
     </nav>
   )
