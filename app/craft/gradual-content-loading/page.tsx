2fcde5b855a34296a602804cd066825808a2b7b9
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-24 18:58:20 +0530

    Gradual Content Loading page metadata updated

--- app/craft/gradual-content-loading/page.tsx
+++ app/craft/gradual-content-loading/page.tsx
@@ -7,7 +7,7 @@ import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
   path: "/craft/gradual-content-loading/",
-  title: "Gradual Content Loading | Crafts | Jitendra Nirnejak",
+  title: "Gradual Content Loading | Craft | Jitendra Nirnejak",
   description:
     "Gradual content loading animation built with React and Framer Motion built by Jitendra Nirnejak",
 })
