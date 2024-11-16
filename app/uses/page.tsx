68f0f6f8d3e8d2ed71991517655fec15caf856a9
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-16 19:43:21 +0530

    Interactive Components -> Interactive Interfaces

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -5,18 +5,28 @@ import { type Metadata } from "next"
 import * as motion from "framer-motion/client"
 import Image from "next/image"
 
-import Photo1 from "assets/uses/IMG_0849.jpeg"
-import Photo2 from "assets/uses/IMG_2056.jpeg"
-import Photo3 from "assets/uses/IMG_2249.jpeg"
-import Photo5 from "assets/uses/IMG_2448.jpeg"
-import Photo6 from "assets/uses/IMG_4289.jpeg"
-import Photo7 from "assets/uses/IMG_4772.jpeg"
-import Photo11 from "assets/uses/IMG_8559.jpeg"
+import MysticallyAwkward from "assets/uses/IMG_0849.jpeg"
+import GoodVibesGreatCoffee from "assets/uses/IMG_2056.jpeg"
+import Photo7 from "assets/uses/IMG_2249.jpeg"
+import TopDownBnW from "assets/uses/IMG_2448.jpeg"
+import Bunny from "assets/uses/IMG_4289.jpeg"
+import BlueTokai from "assets/uses/IMG_4772.jpeg"
+import GirlDrawing from "assets/uses/IMG_4890.jpeg"
+import TacoXbox from "assets/uses/IMG_8559.jpeg"
 import generateMetadata from "utils/seo"
 
 const rotation = [-3, 3, -6, 6]
 
-const images = [Photo1, Photo5, Photo6, Photo7, Photo11, Photo2, Photo3]
+const images = [
+  MysticallyAwkward,
+  TopDownBnW,
+  GirlDrawing,
+  Bunny,
+  BlueTokai,
+  TacoXbox,
+  GoodVibesGreatCoffee,
+  Photo7,
+]
 
 export const metadata: Metadata = generateMetadata({
   path: "/uses/",
@@ -32,10 +42,11 @@ const UsesPage: React.FC = () => {
           Uses
         </h1>
       </section>
-      <section className="relative -mx-4 mt-10 grid w-[calc(100vw+32px)] grid-cols-7 md:mt-20">
+      <section className="relative -ml-4 mt-10 grid w-[calc(100vw+32px)] grid-cols-4 md:mt-20 md:grid-cols-8">
         {images.map((image, index) => (
           <motion.div
             key={index}
+            className={index > 3 ? "-mt-4 md:mt-0" : ""}
             initial={{ opacity: 0, scale: 0.02 }}
             animate={{
               opacity: 1,
@@ -55,6 +66,7 @@ const UsesPage: React.FC = () => {
               rotate: 0,
               zIndex: 5,
               translateY: -30,
+              scale: 1.3,
 
               transition: {
                 type: "spring",
@@ -65,7 +77,7 @@ const UsesPage: React.FC = () => {
               },
             }}
           >
-            <div className="w-32 rounded-3xl bg-white p-2 shadow-xl md:w-60 md:p-3">
+            <div className="w-32 rounded-3xl bg-white p-2 shadow-xl md:w-52 md:p-3">
               <Image
                 width={400}
                 src={image}
