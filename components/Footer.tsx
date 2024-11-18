622ed8c3d372cb1f28edf79fbd5684e2405c2d50
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-18 09:57:11 +0530

    Footer: simplified, RSC, icon removed with text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,43 +1,22 @@
-"use client"
 import * as React from "react"
 
-import { Command } from "akar-icons"
-import { usePathname } from "next/navigation"
-
 const Footer: React.FC = () => {
-  const pathname = usePathname()
-
-  const footerClass = React.useMemo(() => {
-    if (
-      pathname?.includes("blogs") ||
-      pathname?.includes("work") ||
-      pathname?.includes("photos") ||
-      pathname?.includes("uses")
-    ) {
-      return ""
-    } else {
-      return "fixed bottom-0 w-full"
-    }
-  }, [pathname])
-
   return (
-    <footer className={footerClass}>
-      <div className="container">
-        <div className="flex w-full flex-col items-center justify-between gap-4 py-3 text-xs text-zinc-400 md:flex-row">
-          <div className="text-center md:text-left">
-            Designed with ❤️ and a lot of ☕️
-          </div>
-          <div className="hidden md:flex">
-            <p className="flex items-center gap-1 text-zinc-400">
-              <span className="rounded-md bg-zinc-800 p-1.5">
-                <Command size={11} />
-              </span>
-              <span> + </span>
-              <span className="rounded-md bg-zinc-800 px-2 py-1 text-[10px]">
-                K
-              </span>
-            </p>
-          </div>
+    <footer className="container">
+      <div className="flex w-full flex-col items-center justify-between gap-4 py-3 text-xs text-zinc-400 md:flex-row">
+        <div className="text-center md:text-left">
+          Designed with ❤️ and a lot of ☕️
+        </div>
+        <div className="hidden md:flex">
+          <p className="flex items-center gap-1 text-zinc-400">
+            <span className="rounded-md bg-zinc-800 px-1.5 py-1 text-[10px]">
+              ⌘
+            </span>
+            <span> + </span>
+            <span className="rounded-md bg-zinc-800 px-2 py-1 text-[10px]">
+              K
+            </span>
+          </p>
         </div>
       </div>
     </footer>
