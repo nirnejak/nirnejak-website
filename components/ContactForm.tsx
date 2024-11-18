621a7e502468e53dbfa88eada5091ddb95c6f3f0
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-18 11:24:42 +0530

    ContactForm: animated button states

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,8 @@
 "use client"
 import * as React from "react"
 
+import { motion, AnimatePresence } from "framer-motion"
+
 interface ResponseDataType {
   ok: boolean
 }
@@ -14,14 +16,19 @@ declare global {
 const INITIAL_STATE = { name: "", email: "", message: "" }
 
 const ContactForm: React.FC = () => {
-  const [isSending, setIsSending] = React.useState(false)
-  const [isSent, setIsSent] = React.useState(false)
+  const [formState, setFormState] = React.useState("Send")
 
   const [state, setState] = React.useState<{
     name: string
     email: string
     message: string
   }>(INITIAL_STATE)
+
+  const variants = {
+    initial: { opacity: 0, y: -25 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 25 },
+  }
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -31,7 +38,7 @@ const ContactForm: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault()
-    setIsSending(true)
+    setFormState("Sending...")
     window.plausible("Submitted Contact Form")
     fetch("https://formspree.io/f/xgerdbkz", {
       method: "POST",
@@ -40,17 +47,16 @@ const ContactForm: React.FC = () => {
     })
       .then(async (response) => await response.json())
       .then((data: ResponseDataType) => {
-        setIsSending(false)
+        setFormState("Sent!")
         if (data.ok) {
-          setIsSent(true)
           setTimeout(() => {
-            setIsSent(false)
+            setFormState("Send")
             setState(INITIAL_STATE)
-          }, 3000)
+          }, 1500)
         }
       })
       .catch(() => {
-        setIsSending(false)
+        setFormState("Send")
       })
   }
 
@@ -84,10 +90,22 @@ const ContactForm: React.FC = () => {
         required
       />
       <button
-        disabled={isSending || isSent}
+        disabled={formState === "Sending..." || formState === "Sent!"}
         className="rounded-md bg-zinc-50 px-4 py-3 text-center text-sm font-semibold uppercase tracking-wide text-zinc-900 transition-all hover:bg-zinc-200 focus:bg-zinc-200 active:scale-95 disabled:cursor-not-allowed disabled:bg-zinc-400"
       >
-        {isSending ? "SENDING..." : isSent ? "SENT!" : "SEND"}
+        <AnimatePresence mode="popLayout" initial={false}>
+          <motion.span
+            transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={variants}
+            key={formState}
+            className="flex w-full items-center justify-center"
+          >
+            {formState}
+          </motion.span>
+        </AnimatePresence>
       </button>
     </form>
   )
