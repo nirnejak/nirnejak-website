781a61415a2520cad886f2919fdb189e77dd2f52
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-02 16:36:37 +0530

    Blog: year based layout removed

--- utils/data.ts
+++ utils/data.ts
@@ -91,141 +91,130 @@ export const allProjects = {
   ],
 }
 
-export const allBlogs = {
-  2023: [
-    {
-      author: "Jitendra Nirnejak",
-      date: "8 May 2023",
-      description:
-        "Discover the power of SVGs! Learn the basics and advanced techniques for creating and animating stunning, scalable graphics for web",
-      title: "SVG Basics - Elements, Styling, Transformation & Animations",
-      url: "https://www.inkoop.io/blog/svg-basics-element-styling-transformation-animations/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "23 March 2023",
-      description:
-        "An In-Depth Comparison of two popular Node.js Frameworks, and why choosing Fastify may be a better option.",
-      title:
-        "Express.js vs Fastify - In-Depth Comparison of Node.js Frameworks",
-      url: "https://www.inkoop.io/blog/express-vs-fastify-in-depth-comparison-of-node-js-frameworks/",
-    },
-  ],
-  2022: [
-    {
-      author: "Jitendra Nirnejak",
-      date: "25 November 2022",
-      description:
-        "A brief comparison of hstore and JSON data types in PostgreSQL. We learn about the pros and cons of both the data types and when to use them.",
-      title:
-        "Postgres hstore vs JSON - comparing different unstructured data types in PostgreSQL",
-      url: "https://www.draxlr.com/blogs/postgres-hstore-vs-json-comparing-different-unstructured-data-types-in-postgresql/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "11 November 2022",
-      description:
-        "A guide to hstore data type in postgres, details of various postgres hstore operators and functions, when and how to use them.",
-      title: "Postgres HStore data type and how to use it?",
-      url: "https://www.draxlr.com/blogs/postgres-hstore-data-type-and-how-to-use-it/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "15 September 2022",
-      description:
-        "A guide to storing and querying json data in postgres, details of various postgres json operators and functions, when and how to use them.",
-      title: "Postgres JSON data type and how to query it?",
-      url: "https://www.draxlr.com/blogs/postgres-json-data-type-and-how-to-query-it/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "19 July 2022",
-      description:
-        "Draxlr is the first BI tool with full support for PlanetScale Databases. Build Filter, Sort, Summarize and Visualize your MySQL Database from PlanetScale.",
-      title:
-        "Build Dashboards and Setup Slack Alerts on your PlanetScale Database",
-      url: "https://www.draxlr.com/blogs/build-dashboards-and-setup-slack-alerts-on-your-planetScale-database/",
-    },
-  ],
-  2021: [
-    {
-      author: "Jitendra Nirnejak",
-      date: "1 Jun 2021",
-      description:
-        "A closer look into the new image api of Gatsby 3. What's new, what's changed and what's gone.",
-      title: "Gatsby 3 Image - A Deep-dive into the new image API features",
-      url: "https://www.inkoop.io/blog/gatsby-3-image-a-deep-dive-into-the-new-image-api-features/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "11 Feb 2021",
-      description:
-        "Integrate your Shopify Store with Gatsby Site, build e-commerce website using Gatsby and Shopify.",
-      title: "How to integrate Gatsby with Shopify Store",
-      url: "https://www.inkoop.io/blog/how-to-integrate-gatsby-with-shopify-store/",
-    },
-  ],
-  2020: [
-    {
-      author: "Jitendra Nirnejak",
-      date: "19 Nov 2020",
-      description:
-        "A Guide to setting up ESLint for React with Prettier, Pre-commit Hook and Visual Studio Code for better development with linting and code formatting that works with your editor.",
-      title: "Setup ESLint for React with Prettier, pre-commit and VSCode",
-      url: "https://www.inkoop.io/blog/setup-eslint-for-react-with-prettier-pre-commit-and-vscode/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "10 Feb 2020",
-      description:
-        "A guide to building input forms that are easy to use and aesthetically pleasing. UX design tips & tricks to provide better visuals & great user experience.",
-      title:
-        "Forms that don't suck | How to ensure great user experience on forms",
-      url: "https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "31 Jan 2020",
-      description:
-        "A comprehensive list of extremely useful Gatsby Plugins that can help you build, deploy and monitor a great blog.",
-      title: "Must have Gatsby plugins for a great blog",
-      url: "https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/",
-    },
-  ],
-  2019: [
-    {
-      author: "Jitendra Nirnejak",
-      date: "31 Dec 2019",
-      description:
-        "A guide to building custom global vue.js plugins for app-wide accessible components, customs filters and custom directives.",
-      title: "Building custom plugins for Vue.js | Supercharge Development",
-      url: "https://www.inkoop.io/blog/building-custom-plugins-for-vue/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "30 Nov 2019",
-      description:
-        "Vuesax - The all-new Vue Component Library. Why Vuesax? How to Install & Integrate Vuesax? How to customize Vuesax Theme?",
-      title: "Vuesax - The all new Vue Component Library",
-      url: "https://www.inkoop.io/blog/vuesax-the-all-new-vue-component-library/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "30 Oct 2019",
-      description:
-        "A Guide to using JSDoc for generating documentation for react.js applications. Find out the benefits of using JSDoc and using JSDoc with React Component.",
-      title:
-        "A Guide to using JSDoc for React.js | Better Documentation in React",
-      url: "https://www.inkoop.io/blog/a-guide-to-js-docs-for-react-js/",
-    },
-    {
-      author: "Jitendra Nirnejak",
-      date: "24 Sep 2019",
-      description:
-        "AdminJS is a React JS based admin panel for Node JS. We talk about how to set it up and use it in Node JS applications with MongoDB and SQL Databases.",
-      title:
-        "AdminJS - a Modern Admin Panel for Node JS | React based Admin Panel",
-      url: "https://www.inkoop.io/blog/adminjs-a-modern-admin-panel-for-node-js/",
-    },
-  ],
-}
+export const allBlogs = [
+  {
+    author: "Jitendra Nirnejak",
+    date: "8 May 2023",
+    description:
+      "Discover the power of SVGs! Learn the basics and advanced techniques for creating and animating stunning, scalable graphics for web",
+    title: "SVG Basics - Elements, Styling, Transformation & Animations",
+    url: "https://www.inkoop.io/blog/svg-basics-element-styling-transformation-animations/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "23 March 2023",
+    description:
+      "An In-Depth Comparison of two popular Node.js Frameworks, and why choosing Fastify may be a better option.",
+    title: "Express.js vs Fastify - In-Depth Comparison of Node.js Frameworks",
+    url: "https://www.inkoop.io/blog/express-vs-fastify-in-depth-comparison-of-node-js-frameworks/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "25 November 2022",
+    description:
+      "A brief comparison of hstore and JSON data types in PostgreSQL. We learn about the pros and cons of both the data types and when to use them.",
+    title:
+      "Postgres hstore vs JSON - comparing different unstructured data types in PostgreSQL",
+    url: "https://www.draxlr.com/blogs/postgres-hstore-vs-json-comparing-different-unstructured-data-types-in-postgresql/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "11 November 2022",
+    description:
+      "A guide to hstore data type in postgres, details of various postgres hstore operators and functions, when and how to use them.",
+    title: "Postgres HStore data type and how to use it?",
+    url: "https://www.draxlr.com/blogs/postgres-hstore-data-type-and-how-to-use-it/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "15 September 2022",
+    description:
+      "A guide to storing and querying json data in postgres, details of various postgres json operators and functions, when and how to use them.",
+    title: "Postgres JSON data type and how to query it?",
+    url: "https://www.draxlr.com/blogs/postgres-json-data-type-and-how-to-query-it/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "19 July 2022",
+    description:
+      "Draxlr is the first BI tool with full support for PlanetScale Databases. Build Filter, Sort, Summarize and Visualize your MySQL Database from PlanetScale.",
+    title:
+      "Build Dashboards and Setup Slack Alerts on your PlanetScale Database",
+    url: "https://www.draxlr.com/blogs/build-dashboards-and-setup-slack-alerts-on-your-planetScale-database/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "1 Jun 2021",
+    description:
+      "A closer look into the new image api of Gatsby 3. What's new, what's changed and what's gone.",
+    title: "Gatsby 3 Image - A Deep-dive into the new image API features",
+    url: "https://www.inkoop.io/blog/gatsby-3-image-a-deep-dive-into-the-new-image-api-features/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "11 Feb 2021",
+    description:
+      "Integrate your Shopify Store with Gatsby Site, build e-commerce website using Gatsby and Shopify.",
+    title: "How to integrate Gatsby with Shopify Store",
+    url: "https://www.inkoop.io/blog/how-to-integrate-gatsby-with-shopify-store/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "19 Nov 2020",
+    description:
+      "A Guide to setting up ESLint for React with Prettier, Pre-commit Hook and Visual Studio Code for better development with linting and code formatting that works with your editor.",
+    title: "Setup ESLint for React with Prettier, pre-commit and VSCode",
+    url: "https://www.inkoop.io/blog/setup-eslint-for-react-with-prettier-pre-commit-and-vscode/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "10 Feb 2020",
+    description:
+      "A guide to building input forms that are easy to use and aesthetically pleasing. UX design tips & tricks to provide better visuals & great user experience.",
+    title:
+      "Forms that don't suck | How to ensure great user experience on forms",
+    url: "https://www.inkoop.io/blog/how-to-ensure-great-user-experience-on-forms/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "31 Jan 2020",
+    description:
+      "A comprehensive list of extremely useful Gatsby Plugins that can help you build, deploy and monitor a great blog.",
+    title: "Must have Gatsby plugins for a great blog",
+    url: "https://www.inkoop.io/blog/must-have-gatsby-plugins-for-a-great-blog/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "31 Dec 2019",
+    description:
+      "A guide to building custom global vue.js plugins for app-wide accessible components, customs filters and custom directives.",
+    title: "Building custom plugins for Vue.js | Supercharge Development",
+    url: "https://www.inkoop.io/blog/building-custom-plugins-for-vue/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "30 Nov 2019",
+    description:
+      "Vuesax - The all-new Vue Component Library. Why Vuesax? How to Install & Integrate Vuesax? How to customize Vuesax Theme?",
+    title: "Vuesax - The all new Vue Component Library",
+    url: "https://www.inkoop.io/blog/vuesax-the-all-new-vue-component-library/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "30 Oct 2019",
+    description:
+      "A Guide to using JSDoc for generating documentation for react.js applications. Find out the benefits of using JSDoc and using JSDoc with React Component.",
+    title:
+      "A Guide to using JSDoc for React.js | Better Documentation in React",
+    url: "https://www.inkoop.io/blog/a-guide-to-js-docs-for-react-js/",
+  },
+  {
+    author: "Jitendra Nirnejak",
+    date: "24 Sep 2019",
+    description:
+      "AdminJS is a React JS based admin panel for Node JS. We talk about how to set it up and use it in Node JS applications with MongoDB and SQL Databases.",
+    title:
+      "AdminJS - a Modern Admin Panel for Node JS | React based Admin Panel",
+    url: "https://www.inkoop.io/blog/adminjs-a-modern-admin-panel-for-node-js/",
+  },
+]
