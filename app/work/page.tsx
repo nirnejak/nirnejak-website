f80c2983f3995c0315a8b9f2c53acce6a515b5cd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-30 03:35:16 +0530

    Work: side projects

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,9 +2,10 @@ import * as React from "react"
 
 import type { Metadata } from "next"
 
-import AppLink from "@/components/atoms/Link"
 import getMetadata from "@/utils/seo"
+
 import PastEngagements from "@/components/PastEngagements"
+import SideProjects from "@/components/SideProjects"
 
 export const metadata: Metadata = getMetadata({
   path: "/work/",
@@ -23,110 +24,11 @@ const WorkPage: React.FC = () => {
       <section className="container mb-16 mt-10 text-sm md:mt-16">
         <PastEngagements />
       </section>
-      <section className="container mb-16 text-sm md:mt-16">
-        <div className="mb-16 flex flex-col md:flex-row">
-          <p className="mb-3 flex-1 text-zinc-300 md:mt-2">Projects</p>
-          <div className="mx-0 flex flex-1 flex-col gap-1 md:mx-3">
-            {allProjects.work.map((project, index) => (
-              <AppLink
-                key={index}
-                href={project.link}
-                target="_blank"
-                className="hover-bg -mx-3 rounded-md p-3 outline-hidden md:mx-0"
-              >
-                <p className="mb-1.5 font-semibold text-zinc-300">
-                  {project.title}
-                </p>
-                <p className="text-xs font-medium text-zinc-500">
-                  {project.description}
-                </p>
-              </AppLink>
-            ))}
-          </div>
-        </div>
-        <div className="mb-16 flex flex-col md:flex-row">
-          <p className="mb-3 flex-1 text-zinc-300 md:mt-2">
-            Cooking on the side
-          </p>
-          <div className="mx-0 flex flex-1 flex-col gap-1 md:mx-3">
-            {allProjects.side.map((project, index) => (
-              <AppLink
-                key={index}
-                href={project.link}
-                target="_blank"
-                className="hover-bg -mx-3 rounded-md p-3 outline-hidden md:mx-0"
-              >
-                <p className="mb-1.5 font-semibold text-zinc-300">
-                  {project.title}
-                </p>
-                <p className="text-xs font-medium text-zinc-500">
-                  {project.description}
-                </p>
-              </AppLink>
-            ))}
-          </div>
-        </div>
+      <section className="container mb-16 mt-10 text-sm md:mt-16">
+        <SideProjects />
       </section>
     </main>
   )
 }
 
 export default WorkPage
-
-export const allProjects = {
-  side: [
-    {
-      title: "SVG Animations Gallery",
-      description: "A Gallery of SVG Animations built with Framer Motion",
-      link: "https://svg-animations-gallery.vercel.app/",
-    },
-    {
-      title: "TailwindCSS Animations",
-      description: "Copy Paste animations for TailwindCSS",
-      link: "https://tailwindanimations.vercel.app/",
-    },
-    {
-      title: "Interactive Interfaces",
-      description:
-        "A collection of React components animated with Framer Motion",
-      link: "https://interactive-interfaces.vercel.app/",
-    },
-    {
-      title: "React Presentation Maker",
-      description: "Use your React components as presentation slides",
-      link: "https://github.com/nirnejak/react-presentation/",
-    },
-  ],
-  work: [
-    {
-      title: "Draxlr",
-      description: "Product Design and Development",
-      link: "https://www.draxlr.com/",
-    },
-    {
-      title: "Sup Bot",
-      description: "Logo, Branding, Website Design & Product Design.",
-      link: "https://www.supbot.com/",
-    },
-    {
-      title: "Draxlr",
-      description: "Web Animations, Website Design & Development",
-      link: "https://www.draxlr.com/",
-    },
-    {
-      title: "Draxlr",
-      description: "Logo, Branding and Illustrations",
-      link: "https://www.draxlr.com/",
-    },
-    {
-      title: "Inkoop",
-      description: "SEO, Website Design and Development",
-      link: "https://www.inkoop.io/",
-    },
-    {
-      title: "Inkoop",
-      description: "Social Media Content and Technical Content",
-      link: "https://www.instagram.com/inkoopteam/",
-    },
-  ],
-}
---
+++ components/SideProjects.tsx
@@ -0,0 +1,55 @@
+import { link } from "fs"
+import * as React from "react"
+
+interface Props {}
+
+const SideProjects: React.FC<Props> = () => {
+  return (
+    <div className="-mx-3">
+      {projects.map((project, index) => (
+        <a
+          href={project.link}
+          key={index}
+          target="_blank"
+          className="flex items-center gap-2 font-medium hover-bg p-3"
+        >
+          <p className="text-zinc-300">{project.name}</p>
+          <div className="flex-1 border-t border-dashed border-zinc-700" />
+          <p className="flex items-center gap-1 text-zinc-500">
+            {project.work}
+          </p>
+        </a>
+      ))}
+    </div>
+  )
+}
+
+export default SideProjects
+
+const projects = [
+  {
+    name: "SVG Animations Gallery",
+    work: "A Gallery of SVG Animations built with Framer Motion",
+    link: "https://svg-animations-gallery.vercel.app/",
+  },
+  {
+    name: "Interactive Interfaces",
+    work: "UI experiments with React & Framer Motion",
+    link: "https://interactive-interfaces.vercel.app/",
+  },
+  {
+    name: "TailwindCSS Animations",
+    work: "Copy Paste animations for TailwindCSS",
+    link: "https://tailwindanimations.vercel.app/",
+  },
+  {
+    name: "Done Done",
+    work: "A Simple Stupid Interactive Todo List",
+    link: "https://interactive-interfaces.vercel.app/",
+  },
+  {
+    name: "React Presentation Maker",
+    work: "Use your React components as presentation slides",
+    link: "https://github.com/nirnejak/react-presentation/",
+  },
+]
