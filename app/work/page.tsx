750000387334977f87ff18d6372681b9cf384ffd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-30 22:50:25 +0530

    Work: SocialLinks added

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,6 +6,7 @@ import getMetadata from "@/utils/seo"
 
 import PastEngagements from "@/components/PastEngagements"
 import SideProjects from "@/components/SideProjects"
+import SocialIcons from "@/components/SocialIcons"
 
 export const metadata: Metadata = getMetadata({
   path: "/work/",
@@ -16,10 +17,11 @@ export const metadata: Metadata = getMetadata({
 const WorkPage: React.FC = () => {
   return (
     <main>
-      <section className="container mt-32 md:mt-40">
+      <section className="container mt-32 md:mt-40 flex items-center justify-between">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-300 md:text-5xl">
           Work
         </h1>
+        <SocialIcons />
       </section>
       <section className="container mb-16 mt-10 text-sm md:mt-16">
         <PastEngagements />
