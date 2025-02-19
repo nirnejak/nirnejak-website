199cc66a540269a4045ca1542aebfd119b4514bc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-19 18:00:32 +0530

    Interactions: WIP label added

--- app/interactions/page.tsx
+++ app/interactions/page.tsx
@@ -20,9 +20,12 @@ const InteractionsPage: React.FC = () => {
   return (
     <main className="min-h-[77vh]">
       <section className="container mt-32 md:mt-40">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-300 md:text-5xl">
-          Interactions
-        </h1>
+        <div className="flex items-start">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-300 md:text-5xl">
+            Interactions
+          </h1>
+          <span className="text-zinc-300 font-medium">(WIP)</span>
+        </div>
       </section>
       <section className="container my-10">
         <PhotoCards />
