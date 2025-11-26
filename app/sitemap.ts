fcbad14f44db13af42ec0fc56707a252ddc6c2df
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-11-27 02:23:50 +0530

    Sitemap: interactions removed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,12 +25,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
-      url: baseUrl + "/interactions/",
-      lastModified: new Date(),
-      changeFrequency: "weekly" as const,
-      priority: 0.8,
-    },
-    {
       url: baseUrl + "/blogs/",
       lastModified: new Date(),
       changeFrequency: "daily" as const,
