4f3ace7fde90918ce2533ba626aa6c933feeba26
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-04-27 03:08:37 +0530

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,43 +8,36 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = [
     {
       url: `${baseUrl}/`,
-      lastModified: new Date(),
       changeFrequency: "daily" as const,
       priority: 1,
     },
     {
       url: `${baseUrl}/work/`,
-      lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.9,
     },
     {
       url: `${baseUrl}/work/projects/`,
-      lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.9,
     },
     {
       url: `${baseUrl}/blogs/`,
-      lastModified: new Date(),
       changeFrequency: "daily" as const,
       priority: 0.9,
     },
     {
       url: `${baseUrl}/photos/`,
-      lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 0.7,
     },
     {
       url: `${baseUrl}/uses/`,
-      lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 0.7,
     },
     {
       url: `${baseUrl}/contact/`,
-      lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 0.7,
     },
